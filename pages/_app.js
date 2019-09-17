d654246edee09413caae3ca17a4a79d4aa12d694
songjing <songjing@kingsoft.com>
2019-09-17 16:06:39 +0800

    加入自定义App及getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,47 @@
-import App from 'next/app';
+import App, {Container} from 'next/app';
 // import 'antd/dist/antd.css';
+import routeEvent from '../lib/routeEvent';
+import {Router} from 'next/router';
+
 import './index.css'
 
-export default App;
+function makeEvent(type){
+    return (...args) => {
+        console.log(type, ...args);
+    }
+}
+
+for (const key in routeEvent) {
+    if (routeEvent.hasOwnProperty(key)) {
+        const event = routeEvent[key];
+        Router.events.on(event, makeEvent(event));
+    }
+}
+
+
+class MyApp extends App {
+
+    static getInitialProps = async ({Component}) => {
+        let pageProps
+        if(Component.getInitialProps){
+            pageProps = await Component.getInitialProps();
+        }
+        return {
+            pageProps
+        } 
+    }
+
+    render(){
+        const {Component, pageProps} = this.props;
+
+        return (
+            <Container>
+                <div className="Header">header</div>
+                <Component {...pageProps}></Component>
+                <div className="Footer">footer</div>
+            </Container>
+        )
+    }
+}
+
+export default MyApp;
--- pages/author.js
+++ pages/author.js
@@ -1,5 +1,22 @@
 import Rc from '../components/rc';
 import {withRouter} from 'next/router';
+import Link from 'next/link';
+import {Button} from 'antd';
 
-const author = ({router}) => <Rc>{router.query.id}</Rc>;
+const author = (props) => {
+    return (
+        <>
+            <Rc>{props.router.query.id} {props.name}</Rc>
+            <Link href="#hash"><Button>hash</Button></Link>
+        </>
+    )
+} ;
+    
+
+author.getInitialProps = () => {
+    return {
+        name: 'jim'
+    }
+};
+
 export default withRouter(author);
