6866bc7d2c14f8c6a9d444773f009883ad16cf23
songjing <songjing@kingsoft.com>
2019-09-18 16:56:26 +0800

    测试useMemo, memo, useCallback

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect, useReducer, useRef} from 'react'
-import {Button} from 'antd';
+import {useState, useEffect, useReducer, useRef, useMemo, memo, useCallback} from 'react'
+import {Button, Input} from 'antd';
 import Link from 'next/link';
 import getConfig from 'next/config';
 
@@ -10,22 +10,34 @@ function MyCountFunc(props){
     // const [count, setCount] = useState(0)
 
     const spanRef = useRef()
+    const [name, setName] = useState('hello')
     const [count, dispatchCount] = useReducer(countReducer, 0)
+    const config = useMemo(() => (
+        {
+            text: `count is ${count}`,
+            color: count >= 10 ? 'red' : 'blue'
+        }
+    ), [count])
 
     useEffect(() => {
         // 闭包陷阱
         const interval = setInterval(() => {
-            console.log(spanRef);
             dispatchCount({type: 'increase'})
-        }, 1000)
+        }, 2000)
         
         // 组件卸载时执行
         return () => clearInterval(interval)
     }, [])
 
+    const onClickDecrease = useCallback(() => {
+        dispatchCount({type: 'decrease'})
+    }, [])
+
     return (
         <>
-            <span ref={spanRef}>{count}</span>
+            <p><span ref={spanRef}>{count}</span></p>
+            <Input type="text" value={name} onChange={e => setName(e.target.value)}></Input>
+            <ChildEle config={config} decrease={onClickDecrease}></ChildEle>
             <Link href="/author?id=1" as='/author/1'><Button>{props.name}</Button></Link>
         </>
     );
@@ -41,6 +53,12 @@ function countReducer(state, action){
             return state;
     }
 }
+
+const ChildEle = memo(function ({config, decrease}){
+    return <Button onClick={decrease}>
+        <span style={{color: config.color}}>{config.text}</span>
+    </Button>
+})
 
 
 MyCountFunc.getInitialProps = () => {
